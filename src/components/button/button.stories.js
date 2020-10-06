c757f40a0ec803fe00a49f88ffe0062c411a937b
lmachens <leon.machens@googlemail.com>
2020-10-06 14:23:29 +0200

    Add buttons css

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,8 +1,10 @@
 import "./button.css";
 import buttonPrimary from "./button-primary.html";
 import buttonSecondary from "./button-secondary.html";
+import buttonBigContent from "./button-big-content.html";
 
 export default { title: "Components/Button" };
 
 export const primary = () => buttonPrimary;
 export const secondary = () => buttonSecondary;
+export const bigContent = () => buttonBigContent;
