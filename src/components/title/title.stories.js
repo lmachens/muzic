73a0779f006809d4eff11b1ac6703308fa11ca1a
lmachens <leon.machens@googlemail.com>
2020-10-08 12:11:00 +0200

    Replace addon-centered with layout centered

--- src/components/title/title.stories.js
+++ src/components/title/title.stories.js
@@ -1,7 +1,9 @@
 import "./title.css";
 import title from "./title.html";
-import centered from "@storybook/addon-centered/html";
 
-export default { title: "Components/Title", decorators: [centered] };
+export default {
+  title: "Components/Title",
+  parameters: { layout: "centered" },
+};
 
 export const main = () => title;
